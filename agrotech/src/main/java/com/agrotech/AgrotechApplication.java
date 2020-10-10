4facbc692e5347d4759de5a9e32ace292101ddea
KadirAli1 <50846277+KadirAli1@users.noreply.github.com>
2020-10-10 21:09:56 +0200

    Commit  adding data-mysql.sql

--- agrotech/src/main/java/com/agrotech/AgrotechApplication.java
+++ agrotech/src/main/java/com/agrotech/AgrotechApplication.java
@@ -1,10 +1,22 @@
-package com.agrotech;
+package main.java.com.agrotech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgrotechApplication {
+
+	@Configuration
+	@Import({ThymeleafLayoutInterceptorConfig.class})
+	public static class MainConfiguration extends WebMvcConfigurationSupport {
+
+
+	}
+
+	@Bean(name = "passwordEncoder")
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(16);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgrotechApplication.class, args);
